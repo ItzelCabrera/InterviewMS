c5a5f8632e6edf496ca1067c80417d8e40228214
ItzelCabrera <icabrerav1600@alumnno.ipn.mx>
2024-11-22 00:13:28 -0600

    agrega log para revisar promedio

--- src/main/java/com/pt/interviewms/services/InterviewServiceImpl.java
+++ src/main/java/com/pt/interviewms/services/InterviewServiceImpl.java
@@ -157,12 +157,13 @@ public class InterviewServiceImpl implements InterviewService {
                 answer.setScore(items_score.get(1).trim());
                 Answer answerDB = answerRepository.save(answer);
                 if(items_score.get(1).trim().equals("CORRECTO")) promedio++;
-                logger.info("answerUser " + answerDB.getAnswerUser() + "    score " + answerDB.getScore());
+                logger.info("answerUser " + answerDB.getAnswerUser() + "    score " + answerDB.getScore() + " promedio " + promedio);
             }
             InterviewRecord interviewRecord = interviewRecordRepository.findById(interviewJoinScoresDTO.getInterviewId()).
                     orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
             interviewRecord.setScore((promedio * 100L) /2);
             InterviewRecord interviewRecordDB = interviewRecordRepository.save(interviewRecord);
+            logger.info(String.format("interviewScore %d", interviewRecord.getScore()));
         } catch (JsonProcessingException e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"Error al parsear la publicacion de LLM-Service");
         }
